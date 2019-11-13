cb174766a86ebc127d75f3e49b3aeae113349946
Matt Wigdahl <matt@leverpulley.com>
2019-11-13 16:13:39 -0700

    finished comparsion logic and cleaned up files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,73 @@
 var friendsData = [
     {
-        "name": "",
-        "photo":"",
+        "name": "John Lennon",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/8/85/John_Lennon_1969_%28cropped%29.jpg",
         "scores":[
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+          ]
+      
+    },
+    {
+        "name": "Paul McCartney",
+        "photo": "https://de.wikipedia.org/wiki/Paul_McCartney/Diskografie#/media/Datei:Paul_McCartney_black_and_white_2010.jpg",
+        "scores":[
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+          ]
+      
+    },
+    {
+        "name": "George Harrison",
+        "photo": "https://en.wikipedia.org/wiki/George_Harrison#/media/File:George_Harrison_Japan_1991.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+      
+    },
+    {
+        "name": "Ringo Starr",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/3/3b/Ringo_Starr_and_all_his_band_%288470866906%29.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
           ]
       
     }
 ];
 
 module.exports = friendsData;
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,48 @@
 var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
-
   
     app.get("/api/friends", function(req, res) {
       res.json(friendsData);
     });
 
     app.post("/api/friends", function(req, res) {
-        res.json(friendsData);
-      });
+      //setting variables to find match
+      var newFriendsScores = req.body.scores;
+      var scoresArr = [];
+      var bestMatch = 0;
+
+      //loop through friends
+      for(var i = 0; i < friendsData.length; i++){
+        var scoresDiff = 0;
+        console.log("friendsData", friendsData);
+        
+        //loop through friends scores
+        for(var j = 0; j < newFriendsScores.length; j++){
+          //compare each friends scores against the new friend
+          scoresDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendsScores[j])));
+        }
+        
+        scoresArr.push(scoresDiff);
+
+      }
+      
+      //loop through scores to find best match in array
+      for(var i = 0; i < scoresArr.length; i++){
+        if(scoresArr[i] <= scoresArr[bestMatch]){
+          bestMatch = i;
+        }
+      }
+      console.log('bestMatch', bestMatch);
+
+      //return bestMatch data
+      var yourBestFriend = friendsData[bestMatch];
+      res.json(yourBestFriend);
+      
+      //push new friend object to list of friends array
+      friendsData.push(req.body);
+      
 
 
-}
+    });
+  }
